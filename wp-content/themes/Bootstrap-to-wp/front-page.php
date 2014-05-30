540322365326980bb28a872bbe9394007f450849
Kelly Luce <kellyeluce@gmail.com>
2014-05-29 20:30:28 -0700

    loop on front-page to create editable content in wp admin

--- wp-content/themes/Bootstrap-to-wp/front-page.php
+++ wp-content/themes/Bootstrap-to-wp/front-page.php
@@ -3,12 +3,14 @@
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container">
-      
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-          <?php the_content(); ?>
-    
-        <?php endwhile; endif; ?>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post() ;?>
+
+        <h1>Hello, world!</h1>
+        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+        <p><a class="btn btn-primary btn-lg" role="button">Learn more &raquo;</a></p>
+
+      <?php endwhile; endif; ?>
 
       </div>
     </div>
